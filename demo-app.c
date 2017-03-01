51644d6de99fb54bd21b4a5cc506ba86dd6508cb
Jérémie Galarneau <jeremie.galarneau@efficios.com>
2017-03-01 10:01:17 -0500

    Add original notification API usage demo application
    Signed-off-by: Jérémie Galarneau <jeremie.galarneau@efficios.com>

---
+++ demo-app.c
@@ -0,0 +1,370 @@
+/*
+ * demo-app.c
+ *
+ * Demo application demonstrating the intended use of the LTTng notification API
+ *
+ * Copyright 2017 Jérémie Galarneau <jeremie.galarneau@efficios.com>
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+#include <lttng/domain.h>
+#include <lttng/notification.h>
+#include <stdio.h>
+
+int handle_condition(
+		struct lttng_condition *condition,
+		struct lttng_condition_evaluation *condition_evaluation);
+
+int enable_all_events_in_channel(const char *session_name,
+		enum lttng_domain_type domain,
+		const char *channel_name);
+int disable_all_events_in_channel(const char *session_name,
+		enum lttng_domain_type domain,
+		const char *channel_name);
+
+/*
+ * This application demonstrates the intended use of the new LTTng notification
+ * API.
+ *
+ * This application assumes that an existing session, named "my_session", has
+ * already been setup. This session contains a single user space channel,
+ * "my_ust_channel".
+ *
+ * Error checking has been omitted for the sake of brevity.
+ */
+int main(int argc, char **argv)
+{
+	int ret = 0;
+	enum lttng_condition_status condition_status;
+	struct lttng_notification_channel *notification_channel;
+	struct lttng_condition *condition;
+	struct lttng_action *action;
+	struct lttng_trigger *trigger;
+
+	/*
+	 * Open a notification channel.
+	 *
+	 * A notification channel connects the application to the LTTng session
+	 * daemon using a UNIX socket. This "notification channel" can be used
+	 * to listen for various types of notifications.
+	 *
+	 * Multiple independent notification channels can be opened by an
+	 * application, and multiple applications can open notification channels
+	 * to the session daemon.
+	 *
+	 * Within the scope of this feature, a notification channel can be used
+	 * to be notified that two conditions occurred:
+	 *
+	 *   1) Buffer usage high-threshold exceeded
+	 *   2) Buffer usage low-threshold 
+	 *
+	 *
+	 * However, this communication channel may be extended, in the future,
+	 * to allow monitoring applications to be notified of various events,
+	 * such as:
+	 *
+	 *   - creation of new sessions,
+	 *   - activation (enable) of a new event within a session,
+	 *   - the loss of the connection to a relay daemon,
+	 *   - etc.
+	 *
+	 * Note that notification channels are subject to the same security
+	 * rules as the rest of the LTTng control operations. For instance,
+	 * an application must be running as a user (UID) that is part of the
+	 * "tracing" group or be root in order to interact with a root
+	 * session daemon.
+	 */
+	notification_channel = lttng_notification_channel_create();
+
+	/*
+	 * Triggers are a new concept introduced as part of this API.
+	 *
+	 * A trigger performs an Action when a specific Condition is met.
+	 *
+	 * For the sake if this example, we will define and register two
+	 * triggers:
+	 *
+	 * Trigger A:
+	 *   Condition:
+	 *     - The buffer usage of one of the streams of the "my_ust_channel"
+	 *       channel has exceeded 75%.
+	 *   Action:
+	 *     - Emit a notification
+	 *
+	 * Trigger B:
+	 *   Condition:
+	 *     - The buffer usage of all streams of the "my_ust_channel" channel
+	 *       has fallen below 25%.
+	 *   Action:
+	 *     - Emit a notification
+	 *
+	 * Note that both of these conditions are level-triggered.
+	 */
+
+	/* Create Trigger A's condition. */
+	condition = lttng_condition_high_buffer_usage_threshold_create();
+	condition_status = lttng_condition_high_buffer_usage_threshold_set_threshold_percentage(
+			condition, 75);
+	condition_status = lttng_condition_high_buffer_usage_threshold_set_session_name(
+			condition, "my_session");
+
+	/*
+	 * Note that we have created a condition without specifying a channel,
+	 * and without specifying a domain. The only criteria we have set are:
+	 *
+	 *   - A buffer usage threshold of 75%
+	 *   - A target session name, "my_session"
+	 *
+	 * This means that this condition will evaluate to "true" for all
+	 * channels of the session, in every domain (user space, kernel,
+	 * python, etc.).
+	 *
+	 * The threshold can be expressed in bytes or as a percentage.
+	 *
+	 * In our case, since we want to target all channels of the session, it
+	 * makes more sense to express our condition as a percentage since the
+	 * various channels could use buffer sizes that are very different.
+	 */
+
+	/*
+	 * Create the desired "action" to take when the condition defined above
+	 * is met.
+	 *
+	 * In the scope of this feature, the only "action" we are interested in
+	 * is "emit notification".
+	 */
+	action = lttng_action_emit_notification_create();
+
+	/* A trigger associates a condition and an action. */
+	trigger = lttng_trigger_create(condition, action);
+
+	/*
+	 * Register the trigger to LTTng.
+	 *
+	 * LTTng will evaluate the condition specified and trigger the action
+	 * that was associated to it everytime it is met.
+	 */
+	lttng_register_trigger(trigger);
+
+	/*
+	 * Now that we have registered a trigger, a notification will be emitted
+	 * everytime its condition is met.
+	 *
+	 * In order to receive this notification, we must signify our interest
+	 * in these notifications by subscribing to notifications that
+	 * match the same condition.
+	 */
+	lttng_notification_channel_subscribe(notification_channel, condition);
+
+	/* Clean-up */
+	lttng_condition_destroy(condition);
+	lttng_action_destroy(action);
+	lttng_trigger_destroy(trigger);
+
+
+	/* Create Trigger B, register it, and subscribe to its notifications. */
+	condition = lttng_condition_low_buffer_usage_threshold_create();
+	condition_status = lttng_condition_low_buffer_usage_threshold_set_threshold_percentage(
+			condition, 25);
+	action = lttng_action_emit_notification_create();
+	trigger = lttng_trigger_create(condition, action);
+
+	lttng_register_trigger(trigger);
+	lttng_notification_channel_subscribe(notification_channel, condition);
+
+	/* Clean-up */
+	lttng_condition_destroy(condition);
+	lttng_action_destroy(action);
+	lttng_trigger_destroy(trigger);
+
+	for (;;) {
+		struct lttng_notification *notification;
+		enum lttng_notification_channel_status status;
+		struct lttng_condition_evaluation *evaluation;
+
+		/* Receive the next notification. */
+		status = lttng_notification_channel_get_next_notification(
+				notification_channel,
+				&notification,
+				-1 /* no timeout */);
+
+		switch (status) {
+		case LTTNG_NOTIFICATION_CHANNEL_STATUS_OK:
+			break;
+		case LTTNG_NOTIFICATION_CHANNEL_STATUS_TIMEOUT:
+			/*
+			 * Impossible in this case... but present for
+			 * completeness.
+			 */
+			continue;
+		case LTTNG_NOTIFICATION_CHANNEL_STATUS_NOTIFICATIONS_DROPPED:
+			/*
+			 * The session daemon can drop notifications if a
+			 * monitoring application is not consuming the
+			 * notifications fast enough.
+			 *
+			 * The amount of notifications buffered by the session
+			 * daemon is configurable.
+			 */
+			continue;
+		case LTTNG_NOTIFICATION_CHANNEL_STATUS_CLOSED:
+			/*
+			 * The notification channel has been closed by the
+			 * session daemon. This is typically caused by a session
+			 * daemon shutting down (cleanly or because of a crash).
+			 */
+			goto end;
+		default:
+			/* Unhandled conditions / errors. */
+			ret = 1;
+			goto end;
+		}
+
+		/*
+		 * A notification can provide, amongst other things:
+		 *   - the condition that caused this notification to be emitted
+		 *   - the condition evaluation, which provides more specific
+		 *     information on the evaluation of the condition.
+		 *
+		 * In our specific case, the condition will let us know whether
+		 * the high or low threshold was hit.
+		 *
+		 * The condition evaluation provides the buffer usage value
+		 * at the moment the condition was met.
+		 */
+		condition = lttng_notification_get_condition(notification);
+		evaluation = lttng_notification_get_condition_evaluation(
+				notification);
+
+		ret = handle_condition(condition, evaluation);
+		lttng_condition_destroy(condition);
+		lttng_condition_evaluation_destroy(evaluation);
+		lttng_notification_destroy(notification);
+		if (ret != 0) {
+			goto end;
+		}
+	}
+end:
+	lttng_notification_channel_destroy(notification_channel);
+	return ret;
+}
+
+int handle_condition(
+		struct lttng_condition *condition,
+		struct lttng_condition_evaluation *condition_evaluation)
+{
+	int ret = 0;
+	enum lttng_condition_type condition_type;
+
+	condition_type = lttng_condition_get_type(condition);
+
+	switch (condition_type) {
+	case LTTNG_CONDITION_TYPE_HIGH_BUFFER_USAGE_THRESHOLD:
+	{
+		const char *session_name = NULL;
+		const char *channel_name = NULL;
+		enum lttng_domain_type domain = LTTNG_DOMAIN_NONE;
+		uint64_t buffer_usage;
+
+		/*
+		 * Trigger A, which we defined earlier, has been triggered.
+		 *
+		 * We can deduce that the condition evaluation type is
+		 * lttng_condition_evaluation_buffer_usage
+		 *
+		 * The condition evaluation object is used to determine the
+		 * session, domain, and channel of the buffer which triggered
+		 * this notification.
+		 *
+		 * We can also retrieve the buffer's usage at the time of the
+		 * evaluation of the notification.
+		 */
+		lttng_condition_evaluation_buffer_usage_get_buffer_usage_percentage(
+				condition_evaluation, &buffer_usage);
+
+		lttng_condition_evaluation_buffer_usage_get_session_name(
+				condition_evaluation, &session_name);
+
+		lttng_condition_evaluation_buffer_usage_get_domain_type(
+				condition_evaluation, &domain);
+
+		lttng_condition_evaluation_buffer_usage_get_channel_name(
+				condition_evaluation, &channel_name);
+
+		/* Log that this condition happened. */
+		printf("High threshold condition met in session \"%s\", channel \"%s\". The usage at the time of evaluation was of %ull\%",
+				session_name, channel_name, buffer_usage);
+
+		/*
+		 * Since the buffer usage is higher than the threshold we have
+		 * set, we can decide to disable all events associated with the
+		 * channel in which a buffer exceeeded our threshold.
+		 */
+		ret = disable_all_events_in_channel(session_name, domain,
+				channel_name);
+		break;
+	}
+	case LTTNG_CONDITION_TYPE_LOW_BUFFER_USAGE_THRESHOLD:
+	{
+		const char *session_name = NULL;
+		const char *channel_name = NULL;
+		enum lttng_domain_type domain = LTTNG_DOMAIN_NONE;
+
+		/*
+		 * Trigger B, which we defined earlier, has been triggered.
+		 *
+		 * Since the buffer usage is lower than the threshold we have
+		 * set, we can take an arbitrary action. In this case, we decide
+		 * to re-enable all events associated with the channel.
+		 */
+
+		/*
+		 * Retrieve info from condition and condition evaluation
+		 * similarily to the example above.
+		 */
+		ret = enable_all_events_in_channel(session_name, domain,
+				channel_name);
+		break;
+	}
+	default:
+		/* Unexpected condition type. */
+		ret = 1;
+		goto end;
+	}
+end:
+	return ret;
+}
+
+int enable_all_events_in_channel(const char *session_name,
+		enum lttng_domain_type domain,
+		const char *channel_name)
+{
+	/* Not implemented. */
+	return 0;
+}
+
+int disable_all_events_in_channel(const char *session_name,
+		enum lttng_domain_type domain,
+		const char *channel_name)
+{
+	/* Not implemented. */
+	return 0;
+}
+
